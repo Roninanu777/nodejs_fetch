7e521dc8daa46085b4dd6417d5f0cb704cd59052
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-08-31 17:50:46 +0530

    Added get country function

--- src/index.js
+++ src/index.js
@@ -6,29 +6,39 @@ for (let i = 0; i < 25; i++) {
   urls.push(`${base_url}?page=${i + 1}`);
 }
 
-function getData(urls) {
-  return new Promise((resolve, reject) => {
-    let results = [];
-    let expecting = urls.length;
-    urls.forEach((url, index) => {
-      https
-        .get(url, (res) => {
-          let data = "";
-          res.on("data", (chunk) => {
-            data += chunk;
-          });
-          res.on("end", (_) => {
-            results[index] = JSON.parse(data).data;
-            if (--expecting === 0) {
-              resolve(results);
-            }
-          });
-        })
-        .on("error", (err) => reject(err));
-    });
+function getData(callback) {
+  let results = [];
+  let expecting = urls.length;
+  urls.forEach((url, index) => {
+    https
+      .get(url, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", (_) => {
+          results[index] = JSON.parse(data).data;
+          if (--expecting === 0) {
+            callback(results);
+          }
+        });
+        res.on("error", (err) => callback(err));
+      })
+      .on("error", (err) => callback(err));
   });
 }
 
-getData(urls)
-  .then((data) => console.log(data[24]))
-  .catch((err) => console.log(err));
+function getCountryName(code) {
+  getData((results) => {
+    let name = "";
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].length; j++) {
+        if (results[i][j].alpha2Code === code) {
+          name = results[i][j].name;
+          break;
+        }
+      }
+    }
+    console.log(name);
+  });
+}
