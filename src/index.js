54a8cc8c71de6abf5c4882a7f7545b3356846765
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-08-31 13:05:37 +0530

    returned a promise

--- src/index.js
+++ src/index.js
@@ -1,39 +1,34 @@
 const https = require("https");
 let base_url = "https://jsonmock.hackerrank.com/api/countries";
 
-function getAllData(url, page) {
-  let api = `${url}?page=${page}`;
+let urls = [];
+for (let i = 0; i < 25; i++) {
+  urls.push(`${base_url}?page=${i + 1}`);
+}
+
+function getData(urls) {
   return new Promise((resolve, reject) => {
-    https
-      .get(api, (res) => {
-        let data = "";
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-        res.on("end", (_) => {
-          resolve(JSON.parse(data).data);
-        });
-      })
-      .on("error", (err) => reject(err));
+    let results = [];
+    let expecting = urls.length;
+    urls.forEach((url, index) => {
+      https
+        .get(url, (res) => {
+          let data = "";
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
+          res.on("end", (_) => {
+            results[index] = JSON.parse(data).data;
+            if (--expecting === 0) {
+              resolve(results);
+            }
+          });
+        })
+        .on("error", (err) => reject(err));
+    });
   });
 }
 
-function getCountryName() {
-  let result = [];
-  for (let i = 1; i <= 25; i++) {
-    getAllData(base_url, i).then((data) => {
-      result.push(data);
-      console.log(result.length);
-    });
-  }
-  return result;
-}
-
-getCountryName();
-
-// const request = require('request-promise');
-// const urls = ["http://www.google.com", "http://www.example.com"];
-// const promises = urls.map(url => request(url));
-// Promise.all(promises).then((data) => {
-//     // data = [promise1,promise2]
-// });
+getData(urls)
+  .then((data) => console.log(data[24]))
+  .catch((err) => console.log(err));
